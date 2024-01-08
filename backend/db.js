47e82413ad9174cb5a67a2d9cdbf8e95ce74c821
Mohammad Sayadul Hoque <Saidul1000005@gmail.com>
2024-01-09 00:54:10 +0600

    Addition of "Data Validation" using 'express validator" .

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/"
+const mongoURI = "mongodb://localhost:27017/Notebook"
 
 const connectToMongo = async() =>{
         try {
@@ -7,7 +7,6 @@ const connectToMongo = async() =>{
                 mongoose.connect(mongoURI, ()=>{
                         console.log("Connected to Mongoose");
                 }) 
-                console.log('Connected to Mongoose')
             } catch(error) {
                 console.log(error)
                 process.exit()
