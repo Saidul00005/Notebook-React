3c5a4bc87998be4b549711981754225c3e536f67
Mohammad Sayadul Hoque <Saidul1000005@gmail.com>
2024-01-11 22:06:27 +0600

    Addition of "ThunderClient" Collections to manage requests .

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,27 +3,38 @@ const User = require('../models/User')
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
 
-//Create a User using: POST "/api/auth/". Doesn't require Auth
-router.post('/',[
+//Create a User using: POST "/api/authcreateuser". No login required
+router.post('/createuser',[
     body('name',"Enter a valid name").isLength({ min: 3 }),
     body('email','Enter a valid email').isEmail(),
     body('password',"Password needs to be at least 5 characters").isLength({ min: 5 }),
     
-], (req,res) =>{
+], async (req,res) =>{
+        //If there are errors, return bad request and errors
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
           return res.status(400).json({ errors: errors.array() });
         }
+        try {
+                //Check whether the user with this email already exist
+                let user = await User.findOne({email: req.body.email});
+                if(user){
+                  return res.status(400).json({error:"Sorry a user with this email already exists"})
+                }
+                user = await User.create({
+                name: req.body.name,
+                password: req.body.password,
+                email: req.body.email
+                
+              })
+              res.json(user)
+
+        } catch (error) {
+          console.error(error.message)
+          res.status(500).send('Some error')
+        }
         
-   User.create({
-        name: req.body.name,
-        password: req.body.password,
-        email: req.body.email
-        
-      }).then(user => res.json(user))
-      .catch(err=>{console.log(err)
-        res.json({error:"Please enter a unique value.",message:err.message})})
-      
+     
 })
 
 module.exports = router
