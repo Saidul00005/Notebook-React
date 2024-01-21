f6129de9267227b037f20b34be847db456f0acb2
Mohammad Sayadul Hoque <Saidul1000005@gmail.com>
2024-01-21 22:47:42 +0600

    Addition of "Navbar" and " Routes" .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import About from './Components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is Notebook.</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/about" element={<About/>} />
+
+        </Routes>
+      </Router>
+    </>
   );
 }
 
