07ecf55d5f3da8a89f4a5684b3aafd8ef17e0a3b
Mohammad Sayadul Hoque <Saidul1000005@gmail.com>
2024-01-26 21:42:02 +0600

    Addition of fetch method for fetching notes from Context .

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import noteContext from "../Context/notes/noteContext"
 
 const Home = () => {
+  const context = useContext(noteContext);
+  const {notes,setNotes} = context;
   return (
     <div>
       <div className="container my-3">
@@ -22,9 +25,12 @@ const Home = () => {
           <button type="submit" class="btn btn-primary">Submit</button>
         </form>
       </div>
-      
+
       <div className="container my-3">
         <h2>Your Notes</h2>
+        {notes.map((note)=>{
+          return note.title;
+        })}
       </div>
     </div>
   )
--- src/Context/notes/NoteState.js
+++ src/Context/notes/NoteState.js
@@ -1,14 +1,28 @@
 import NoteContext from "./noteContext";
+import { useState } from "react";
 
 
-const NoteState =(props) =>{
+const NoteState = (props) => {
+        const notesInitial = [
+                {
+                        "_id": "65ad32a2e3113b8f596c4d45",
+                        "user": "65ad2e043fb22fd8b36d116a",
+                        "title": "Updated title",
+                        "description": "Updated description",
+                        "tag": "Updated tag",
+                        "date": "2024-01-21T15:05:06.191Z",
+                        "__v": 0
+                }
+        ]
 
-        return(
-                <NoteContext.Provider value={{}}>
+        const [notes, setNotes] = useState(notesInitial)
+        
+        return (
+                <NoteContext.Provider value={{notes,setNotes}}>
                         {props.children}
                 </NoteContext.Provider>
         )
 }
 
 
-export default NoteState ;
+export default NoteState;
